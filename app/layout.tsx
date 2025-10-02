8c9236302339df7654142a8e5265145aa319b71d
vulgoney7 <lllucas.noka@gmail.com>
2025-10-02 19:35:08 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
 import type React from "react";
-// A importação está correta
-import { GeistSans } from 'geist/font/sans';
+import { GeistSans } from "geist/font/sans";
 import { Playfair_Display } from "next/font/google";
+import Script from "next/script"; // Import necessário para injetar scripts
 import "./globals.css";
 
-// CORREÇÃO: Não chame GeistSans como uma função.
-// Ela já é o objeto da fonte que precisamos.
-// Apenas a fonte do Google Fonts (Playfair) precisa ser chamada como função.
+// Fonte Playfair (Google)
 const playfair = Playfair_Display({
   subsets: ["latin"],
   variable: "--font-playfair",
@@ -23,12 +21,55 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    // CORREÇÃO: Use a variável da GeistSans diretamente.
     <html lang="es" className={`${GeistSans.variable} ${playfair.variable}`}>
+      <head>
+        {/* PIXEL FACEBOOK */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1831361924129623');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1831361924129623&ev=PageView&noscript=1"
+          />
+        </noscript>
+
+        {/* SCRIPT UTMIFY - Pixel */}
+        <Script id="utmify-pixel" strategy="afterInteractive">
+          {`
+            window.pixelId = "68defa17a8eb6a7d4aa98828";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
+
+        {/* SCRIPT UTMIFY - UTM Tracker */}
+        <Script
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-subids
+          async
+          defer
+        />
+      </head>
       <body className="font-sans antialiased bg-background text-foreground">
         {children}
       </body>
     </html>
   );
 }
-
