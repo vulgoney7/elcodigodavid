b3235db3a8053da2375f8b599abab0b6327ba81a
vulgoney7 <lllucas.noka@gmail.com>
2025-10-03 01:06:23 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1831361924129623');
+            fbq('init', '1330327481864816');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -43,7 +43,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1831361924129623&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1330327481864816&ev=PageView&noscript=1"
           />
         </noscript>
 
